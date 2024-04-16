636c6c8d5939b66a5c4401266f868be5c0ba795c
LeoTheLemon <167257068+LeoTheLemon@users.noreply.github.com>
2024-04-17 01:42:05 +0300

    Add files via upload

---
+++ zesty-download.py
@@ -0,0 +1,60 @@
+from pytube import YouTube
+import customtkinter
+panel = 0
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+vid =""
+#main-GUI
+app = customtkinter.CTk()
+app.title("Zesty Download")
+app.geometry("500x300")
+
+def Download():
+ pass
+def getV():
+    if link.get() != "":
+     ytLink = link.get()
+     ytThing = YouTube(ytLink)
+     screen = customtkinter.CTkToplevel(app)
+     screen.title("Download")
+     screen.geometry("500x300")
+     resolution =[str(i.split("p")[0]) for i in (list(dict.fromkeys([i.resolution for i in ytThing.streams if i.resolution])))]
+     def select():
+       global vid
+       vid = Options.get()
+
+     def Down():
+       global vid
+       video = ytThing.streams.get_by_resolution(vid)
+       video.download()
+       done = customtkinter.CTkLabel(screen,text="Done!")
+       done.pack(pady= 10)                             
+        
+
+     Dbutton = customtkinter.CTkButton(screen, text= "Download", command= Down)
+     Dbutton.pack(pady= 60)
+     Options = customtkinter.CTkOptionMenu(screen, values= resolution, command= select)
+     Options.pack(pady=40)
+       
+    else:
+     screen = customtkinter.CTkToplevel(app)
+     screen.title("Download")
+     screen.geometry("500x300")
+     NoLink = customtkinter.CTkLabel(screen,text= "ERROR please Enter a video link")
+     NoLink.pack(pady= 40)
+
+#link Input
+urlVar = customtkinter.StringVar()
+link = customtkinter.CTkEntry(app,placeholder_text= "Place link here",placeholder_text_color="blue",textvariable= urlVar,width= 300, height= 50)  
+link.pack(pady= 40)
+
+button = customtkinter.CTkButton(app,text= "submit",height= 50, width= 200,command= getV)
+button.pack(pady= 60)
+
+
+
+
+
+app.mainloop()
